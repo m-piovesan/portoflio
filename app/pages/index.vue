037c6afa24af959cb086a25335dbf76bc56a0580
Matheus Piovesan <matheusmoraespiovesan@gmail.com>
2025-12-16 09:56:06 -0300

    feat: start

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex w-full h-[20vh] bg-red-400 items-end justify-center">
+        <div class="rounded-full size-40 bg-green-300 -mb-12">
+        </div>
+    </div>
+</template>
